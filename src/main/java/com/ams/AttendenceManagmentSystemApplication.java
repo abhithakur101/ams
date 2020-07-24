f94f2f5f9e79ada70fd99c80ac9b62ca40c661a8
Your Name <abhithakur1011@gmail.com>
2020-07-24 15:13:28 +0530

    first commit

---
+++ src/main/java/com/ams/AttendenceManagmentSystemApplication.java
@@ -0,0 +1,13 @@
+package com.ams;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AttendenceManagmentSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AttendenceManagmentSystemApplication.class, args);
+	}
+
+}
