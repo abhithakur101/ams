a54bdba9d9a50490e81792ff5f4a293d52d9376a
AbdulBariishere <abdulbariishere@gmail.com>
2020-07-24 18:52:41 +0530

    method and attendance controller added

---
+++ src/main/java/com/ams/restcontroller/AttendenceController.java
@@ -0,0 +1,23 @@
+package com.ams.restcontroller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/attendence")
+public class AttendenceController {
+    public void submitAttendence() {
+    }
+
+    public void attendenceCorrection() {
+    }
+
+    public void updateAttendence() {
+    }
+
+    public void listAttendence() {
+    }
+
+    public void approveAttendence() {
+    }
+}
--- src/main/java/com/ams/restcontroller/EmployeeController.java
+++ src/main/java/com/ams/restcontroller/EmployeeController.java
@@ -1,7 +1,29 @@
 package com.ams.restcontroller;
 
 import com.ams.modal.Employee;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
+@CrossOrigin("*")
+@RestController
+@RequestMapping("/employee")
 public class EmployeeController {
+    public void getEmployees() {
+    }
 
+    public void getEmployee() {
+
+    }
+
+    public void addEmployee() {
+    }
+
+    public void UpdateEmployee() {
+    }
+
+    public void specificEmployee() {
+    }
 }
