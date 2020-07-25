f0c8bfd94fe253236431e49aa8bc81066a7b396b
Your Name <abhithakur1011@gmail.com>
2020-07-25 11:55:02 +0530

    add modal class

--- src/main/java/com/ams/restcontroller/EmployeeController.java
+++ src/main/java/com/ams/restcontroller/EmployeeController.java
@@ -1,11 +1,8 @@
 package com.ams.restcontroller;
 
-import com.ams.modal.Employee;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 @CrossOrigin("*")
 @RestController
